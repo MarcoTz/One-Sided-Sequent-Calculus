7b6a5b90d47a51e0635c445028969e853f2dd87f
MarcoTz <marco.tzschentke@gmail.com>
2024-03-18 14:09:46 +0100

    udpdated web compile with requirements

--- build_web.sh
+++ build_web.sh
@@ -1,2 +1,7 @@
+#requires ghc wasm backend
+# https://gitlab.haskell.org/ghc/ghc-wasm-meta
+# https://ghc.gitlab.haskell.org/ghc/doc/users_guide/wasm.html
+source ~/.ghc-wasm/env
 cabal --with-compiler=wasm32-wasi-ghc --with-hc-pkg=wasm32-wasi-ghc-pkg --with-hsc2hs=wasm32-wasi-hsc2hs build web-app 
 wasmtime run dist-newstyle/build/wasm32-wasi/ghc-9.9.20240309/one-sided-sequent-0.1/x/web-app/build/web-app/web-app.wasm
+
