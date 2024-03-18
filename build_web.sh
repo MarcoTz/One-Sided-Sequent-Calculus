4d0e951874ee7dce98fdebfca8647fa7477f53bb
MarcoTz <marco.tzschentke@gmail.com>
2024-03-18 14:53:41 +0100

    added running test-suite from wasm

--- build_web.sh
+++ build_web.sh
@@ -2,6 +2,12 @@
 # https://gitlab.haskell.org/ghc/ghc-wasm-meta
 # https://ghc.gitlab.haskell.org/ghc/doc/users_guide/wasm.html
 source ~/.ghc-wasm/env
-cabal --with-compiler=wasm32-wasi-ghc --with-hc-pkg=wasm32-wasi-ghc-pkg --with-hsc2hs=wasm32-wasi-hsc2hs build web-app 
-wasmtime run dist-newstyle/build/wasm32-wasi/ghc-9.9.20240309/one-sided-sequent-0.1/x/web-app/build/web-app/web-app.wasm
 
+if [ $1 -eq "test-suite"] 
+then
+  cabal --with-compiler=wasm32-wasi-ghc --with-hc-pkg=wasm32-wasi-ghc-pkg --with-hsc2hs=wasm32-wasi-hsc2hs build test-suite 
+  wasmtime run --mapdir /::$PWD dist-newstyle/build/wasm32-wasi/ghc-9.9.20240309/one-sided-sequent-0.1/x/test-suite/build/test-suite/test-suite.wasm
+else
+  cabal --with-compiler=wasm32-wasi-ghc --with-hc-pkg=wasm32-wasi-ghc-pkg --with-hsc2hs=wasm32-wasi-hsc2hs build web-app 
+  wasmtime run dist-newstyle/build/wasm32-wasi/ghc-9.9.20240309/one-sided-sequent-0.1/x/web-app/build/web-app/web-app.wasm
+fi
