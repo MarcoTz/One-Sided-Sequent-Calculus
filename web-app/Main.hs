b30610211f37820bbc56a1c19b4aff6965aeb986
MarcoTz <marco.tzschentke@gmail.com>
2024-03-20 17:30:25 +0100

    updated web app WIP

--- web-app/Main.hs
+++ web-app/Main.hs
@@ -4,13 +4,23 @@ import Driver.Definition
 import Driver.Driver 
 import Environment
 import Pretty.Driver ()
+import Pretty.Errors()
+import Callback
+import JSBits
+import GHC.JS.Prim
+
+foreign import javascript "((c => { globalCompiler = c})"
+  setCompiler :: CallBack (JSVal -> IO ()) -> IO ()
 
 srcContents ::  String 
 srcContents = "module Fun\ndata Fun(a:+,b:-):- \n{ Ap(a,b)\n}\n\nid :: forall X. Fun(X,X) : +;\nid := case { Ap(x,a) => <x | + | a> };\n\nid2 :: Forall X. Fun(X,X) : +;\n\nid2 := id;"
 
-main :: IO()
+runProg :: JSVal -> IO () 
+runProg val = do 
+  let progSource = fromJSString val
+  let drvSt = MkDriverState False emptyEnv
+  res <- runDriverM drvSt (runStr mn)
+
+main :: IO () 
 main = do
-  let drvSt = MkDriverState True emptyEnv
-  res <- runDriverM drvSt (runStr srcContents)
-  print res
- 
+  setCompiler runProg  
