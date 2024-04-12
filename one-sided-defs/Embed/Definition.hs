d392444794612bb3beab520a15494d3f86523e4d
MarcoTz <marco.tzschentke@gmail.com>
2024-04-12 15:01:27 +0200

    renamed files to purs

--- one-sided-defs/Embed/Definition.hs
+++
@@ -1,7 +0,0 @@
-module Embed.Definition (
-  Embed,
-  embed
-)where 
-
-class Embed a b where 
-  embed :: a -> b 
--- one-sided-defs/Pretty/Environment.hs
+++
@@ -1,7 +0,0 @@
-module Pretty.Environment () where 
-
-import Environment 
-import Pretty.Kinded ()
-
-instance Show Environment where 
-  show (MkEnv defs) = show defs 
