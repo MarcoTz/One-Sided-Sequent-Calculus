dcb14f896bb73dac6c46c0ddbcc8bcaddcedfbb6
MarcoTz <marco.tzschentke@gmail.com>
2024-05-23 13:36:22 +0200

    updated maybe

--- one-sided-examples/src/ImportLibs.js
+++ one-sided-examples/src/ImportLibs.js
@@ -20,6 +20,7 @@ export const maybeSrc = `
 module Maybe
 
 import Fun 
+import Bool
 
 data Maybe(a:+) { 
   Nothing,
@@ -28,6 +29,18 @@ data Maybe(a:+) {
 
 maybe :: forall a b. b -> (a -> b) -> Maybe(a) -> b
 maybe := \\b. \\f. \\a. mu c. < a | CBV | case { Nothing => <b|CBV|c>, Just(a) => <f [a]|CBV|c> }>
+
+isJust :: forall a. Maybe(a) -> Bool
+isJust := \\a. mu c. < a | CBV | case { Nothing => <False|CBV|c>, Just(a) => <True|CBV|c> }>
+
+isNothing :: forall a. Maybe(a) -> Bool
+isNothing := \\a. mu c. <a | CBV | case { Nothing => <True|CBV|c>, Just(a) => <False|CBV|c>}>
+
+fromJust :: forall a. Maybe(a) -> a 
+fromJust := \\a. mu c. <a|CBV|case {Nothing => error "expected Just", Just(x) => <x|CBV|c>}>
+
+fromMaybe :: forall a. a -> Maybe(a) -> a
+fromMaybe := \\a. \\ma. mu c. <ma|CBV|case {Nothing => <a|CBV|c>, Just(a1)=><a1|CBV|c>}>
 `;
 
 export const natSrc = `
--- one-sided-examples/stdlib/Maybe.os
+++ one-sided-examples/stdlib/Maybe.os
@@ -1,6 +1,7 @@
 module Maybe
 
 import Fun 
+import Bool
 
 data Maybe(a:+) { 
   Nothing,
@@ -9,3 +10,15 @@ data Maybe(a:+) {
 
 maybe :: forall a b. b -> (a -> b) -> Maybe(a) -> b
 maybe := \b. \f. \a. mu c. < a | CBV | case { Nothing => <b|CBV|c>, Just(a) => <f [a]|CBV|c> }>
+
+isJust :: forall a. Maybe(a) -> Bool
+isJust := \a. mu c. < a | CBV | case { Nothing => <False|CBV|c>, Just(a) => <True|CBV|c> }>
+
+isNothing :: forall a. Maybe(a) -> Bool
+isNothing := \a. mu c. <a | CBV | case { Nothing => <True|CBV|c>, Just(a) => <False|CBV|c>}>
+
+fromJust :: forall a. Maybe(a) -> a 
+fromJust := \a. mu c. <a|CBV|case {Nothing => error "expected Just", Just(x) => <x|CBV|c>}>
+
+fromMaybe :: forall a. a -> Maybe(a) -> a
+fromMaybe := \a. \ma. mu c. <ma|CBV|case {Nothing => <a|CBV|c>, Just(a1)=><a1|CBV|c>}>
